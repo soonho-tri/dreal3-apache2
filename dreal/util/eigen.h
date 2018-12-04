28a6093e8d667a8bab4c3baeceecac556c5fe216
Soonho Kong <soonho.kong@tri.global>
2018-12-04 04:06:36 -0500

    feat(symbolic): Add autodiffxd.h

--- dreal/util/eigen.h
+++ dreal/util/eigen.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "dreal/util/autodiffxd.h"
 #include "dreal/util/eigen_autodiff_types.h"
 #include "dreal/util/eigen_autodiff_overloads.h"
 #include "dreal/util/eigen_types.h"
