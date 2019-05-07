1c720fbf85def88092dc27cef6c3ebbcd2270ed4
Soonho Kong <soonho.kong@tri.global>
2019-05-07 12:56:45 -0400

    refactor(solver): Add branch and icp_stat

---
+++ dreal/solver/branch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "dreal/util/box.h"
+
+namespace dreal {
+
+/// Partitions @p box into two sub-boxes and add them into the @p
+/// stack. It traverses only the variables enabled by @p bitset, to find a
+/// branching dimension.
+///
+/// @returns true if it finds a branching dimension and adds boxes to the @p
+/// stack.
+/// @returns false if it fails to find a branching dimension.
+bool Branch(const Box& box, const ibex::BitSet& bitset,
+            const bool stack_left_box_first,
+            std::vector<std::pair<Box, int>>* const stack);
+
+}  // namespace dreal
