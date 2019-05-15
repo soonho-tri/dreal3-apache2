b18801e21e2b73308ca75b35a63a4f004530a831
Soonho Kong <soonho.kong@tri.global>
2019-05-15 14:48:11 -0400

    feat(util/cds.h): Add CDS-related utilities

---
+++ dreal/util/cds.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <memory>
+
+#include <cds/container/treiber_stack.h>
+#include <cds/gc/hp.h>
+#include <cds/init.h>
+
+namespace dreal {
+
+using gc_type = cds::gc::HP;
+
+template <typename T>
+using Stack = cds::container::TreiberStack<gc_type, T>;
+
+class CdsScopeGuard {
+ public:
+  explicit CdsScopeGuard(bool use) : use_{use} {
+    if (use_) {
+      cds::threading::Manager::attachThread();
+    }
+  }
+  ~CdsScopeGuard() {
+    if (use_) {
+      cds::threading::Manager::detachThread();
+    }
+  }
+
+ private:
+  const bool use_{};
+};
+
+class CdsInit {
+ public:
+  explicit CdsInit(bool use_lock_free_container) {
+    // Initialize libcds
+    cds::Initialize();
+    hpGC_ = std::make_unique<gc_type>();
+    if (use_lock_free_container) {
+      cds::threading::Manager::attachThread();
+    }
+  }
+  ~CdsInit() {
+    // Terminate libcds
+    cds::Terminate();
+  }
+
+ private:
+  // Initialize Hazard Pointer singleton
+  std::unique_ptr<gc_type> hpGC_;
+};
+
+}  // namespace dreal
