4dc3487f1db6fce3e0ca4b8c703b72df49ffc9b4
Soonho Kong <soonho.kong@tri.global>
2019-05-22 21:19:58 -0400

    feat(contractor): Add ContractorIbexFwdbwdMt

---
+++ dreal/contractor/contractor_ibex_fwdbwd_mt.cc
@@ -0,0 +1,69 @@
+#include "dreal/contractor/contractor_ibex_fwdbwd_mt.h"
+
+#include <utility>
+
+#include "dreal/util/logging.h"
+
+using std::make_unique;
+using std::ostream;
+using std::thread;
+using std::unique_ptr;
+
+namespace dreal {
+
+ContractorIbexFwdbwdMt::ContractorIbexFwdbwdMt(Formula f, const Box& box,
+                                               const Config& config)
+    : ContractorCell{Contractor::Kind::IBEX_FWDBWD,
+                     ibex::BitSet::empty(box.size()), config},
+      f_{std::move(f)},
+      config_{config} {
+  DREAL_LOG_DEBUG("ContractorIbexFwdbwdMt::ContractorIbexFwdbwdMt");
+  GetCtcOrCreate(box);
+}
+
+ContractorIbexFwdbwd* ContractorIbexFwdbwdMt::GetCtcOrCreate(
+    const Box& box) const {
+  thread_local const std::thread::id id{std::this_thread::get_id()};
+  ContractorIbexFwdbwd* ctc{nullptr};
+  if (ctc_map_.find_fn(id, [&ctc](const unique_ptr<ContractorIbexFwdbwd>& v) {
+        ctc = v.get();
+      })) {
+    return ctc;
+  }
+  auto ctc_unique_ptr = make_unique<ContractorIbexFwdbwd>(f_, box, config_);
+  ctc = ctc_unique_ptr.get();
+  ctc_map_.insert(id, std::move(ctc_unique_ptr));
+  return ctc;
+}
+
+ContractorIbexFwdbwd* ContractorIbexFwdbwdMt::GetCtc() const {
+  thread_local const std::thread::id id{std::this_thread::get_id()};
+  ContractorIbexFwdbwd* ctc{nullptr};
+  if (ctc_map_.find_fn(id, [&ctc](const unique_ptr<ContractorIbexFwdbwd>& v) {
+        ctc = v.get();
+      })) {
+    return ctc;
+  }
+  return ctc;
+}
+
+void ContractorIbexFwdbwdMt::Prune(ContractorStatus* cs) const {
+  ContractorIbexFwdbwd* const ctc{GetCtcOrCreate(cs->box())};
+  if (ctc) {
+    return ctc->Prune(cs);
+  }
+}
+
+Box::Interval ContractorIbexFwdbwdMt::Evaluate(const Box& box) const {
+  return GetCtc()->Evaluate(box);
+}
+
+ostream& ContractorIbexFwdbwdMt::display(ostream& os) const {
+  ContractorIbexFwdbwd* const ctc{GetCtc()};
+  if (ctc) {
+    return ctc->display(os);
+  } else {
+    return os << "IbexFwdbwd()";
+  }
+}
+}  // namespace dreal
---
+++ dreal/contractor/contractor_ibex_fwdbwd_mt.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <memory>
+#include <ostream>
+#include <thread>
+
+#include <cuckoohash_map.hh>
+
+#include "dreal/contractor/contractor_cell.h"
+#include "dreal/contractor/contractor_ibex_fwdbwd.h"
+#include "dreal/contractor/contractor_status.h"
+#include "dreal/symbolic/symbolic.h"
+#include "dreal/util/box.h"
+
+namespace dreal {
+
+/// Multi-thread version of ContractorIbexFwdbwd contractor.
+class ContractorIbexFwdbwdMt : public ContractorCell {
+ public:
+  /// Deleted default constructor.
+  ContractorIbexFwdbwdMt() = delete;
+
+  /// Constructs IbexFwdbwdMt contractor using @p f and @p box.
+  ContractorIbexFwdbwdMt(Formula f, const Box& box, const Config& config);
+
+  /// Deleted copy constructor.
+  ContractorIbexFwdbwdMt(const ContractorIbexFwdbwdMt&) = delete;
+
+  /// Deleted move constructor.
+  ContractorIbexFwdbwdMt(ContractorIbexFwdbwdMt&&) = delete;
+
+  /// Deleted copy assign operator.
+  ContractorIbexFwdbwdMt& operator=(const ContractorIbexFwdbwdMt&) = delete;
+
+  /// Deleted move assign operator.
+  ContractorIbexFwdbwdMt& operator=(ContractorIbexFwdbwdMt&&) = delete;
+
+  ~ContractorIbexFwdbwdMt() override = default;
+
+  void Prune(ContractorStatus* cs) const override;
+
+  /// Evaluates the constraint using the input @p box and returns the
+  /// result.
+  Box::Interval Evaluate(const Box& box) const;
+
+  std::ostream& display(std::ostream& os) const override;
+
+ private:
+  ContractorIbexFwdbwd* GetCtc() const;
+
+  ContractorIbexFwdbwd* GetCtcOrCreate(const Box& box) const;
+
+  const Formula f_;
+  const Config config_;
+
+  mutable cuckoohash_map<std::thread::id, std::unique_ptr<ContractorIbexFwdbwd>>
+      ctc_map_;
+};
+
+}  // namespace dreal
